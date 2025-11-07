93ff3d0a350547d663dbc56fecd6e17ddc6757de
joonsol <71648638+joonsol@users.noreply.github.com>
2025-11-07 19:47:31 +0900

    admin post user update

--- frontend/src/pages/admin/AdminPosts.jsx
+++ frontend/src/pages/admin/AdminPosts.jsx
@@ -1,30 +1,56 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { fetchAdminPosts, patchAdminPost } from "../../api/adminApi";
 import AdminPostList from "../../components/admin/AdminPostsList";
-import AdminFilter from "../../components/admin/AdminFilter";
+import AdminPostFilter from "../../components/admin/AdminPostFilter";
 const AdminPosts = () => {
-  const [list, setList] = useState([]);
-  const [query, setQuery] = useState({
-    page: 1,
-    size: 10,
-    status: "",
-    q: "",
-    user: "",
-  });
+ const [list, setList] = useState([]);
+ const [query, setQuery] = useState({
+  page: 1,
+  size: 10,
+  status: "",
+  q: "",
+  user: "",
+ });
 
-  useEffect(() => {
-    (async () => {
-      const items = await fetchAdminPosts(query);
-      setList(items);
-    })();
-  }, [query]);
+ useEffect(() => {
+  (async () => {
+   const items = await fetchAdminPosts(query);
+   setList(items);
+  })();
+ }, [query]);
 
-  return (
-    <div>
-      <AdminFilter/>
-      <AdminPostList  items={list} />
-    </div>
-  );
+ const handleApprove = async (id) => {
+  try {
+   const updated = await patchAdminPost(id, { status: "approved" });
+
+   setList((prev) => prev.map((it) => (it._id === id ? updated : it)));
+  } catch (error) {
+   console.error("승인 처리 실패", error);
+  }
+ };
+ const handleReject = async (id) => {
+  try {
+   const updated = await patchAdminPost(id, { status: "rejected" });
+
+   setList((prev) => prev.map((it) => (it._id === id ? updated : it)));
+  } catch (error) {
+   console.error("승인 처리 실패", error);
+  }
+ };
+
+ return (
+  <div>
+   <AdminPostFilter
+   value={query}
+   onChange={setQuery}
+   />
+   <AdminPostList
+    items={list}
+    onApprove={handleApprove}
+    onReject={handleReject}
+   />
+  </div>
+ );
 };
 
 export default AdminPosts;
